bc0941b7d5cd279f0ecd605e91e5b8aa2c242a2a
Impuls3iv <sammy.unterman@gmail.com>
2016-07-11 22:55:31 -0400

    did stretch where you need to put the full details button too

--- app/assets/javascripts/pokemons.js
+++ app/assets/javascripts/pokemons.js
@@ -13,9 +13,30 @@ $(function(){
       var source = $('#entry-template').html(); //get the template html
       var template = Handlebars.compile(source); //compile template to use
       var output = template(responseData); //load the data into the template
+      $('#pokebox').html('');
+      $('#pokebox2').html('');
       $('#pokebox').html(output); //append the template to the div
     }).fail(function(){
-      console.log("You fucked something up for sure...")
+      console.log("You fucked something up for sure...");
+    });
+  });
+
+  $('.pokelink2').on('click', function(e){
+    e.preventDefault();
+
+    $.ajax({
+      url: $(this).attr('href'),
+      method: 'GET',
+      data: { },
+      dataType: 'json'
+    }).done(function(responseData){
+      $('#pokebox').html('');
+      var source = $('#entry-template2').html();
+      var template = Handlebars.compile(source);
+      var output = template(responseData);
+      $('#pokebox2').html(output);
+    }).fail(function(){
+      console.log("something messed up");
     });
   });
 });
